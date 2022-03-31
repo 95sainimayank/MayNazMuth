cbb14eb28c204207868b2cbd91c6ef004d6c97ee
95sainimayank <95sainimayank@gmail.com>
2022-03-30 20:54:08 -0700

    Added Main page buttons

---
+++ MayNazMuth/BookFlightWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MayNazMuth {
+    /// <summary>
+    /// Interaction logic for BookFlightWindow.xaml
+    /// </summary>
+    public partial class BookFlightWindow : Window {
+        public BookFlightWindow() {
+            InitializeComponent();
+        }
+    }
+}
