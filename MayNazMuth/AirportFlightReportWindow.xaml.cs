3e0b944f46914d1b79033570983a26ce89a0d8fc
nazaninbinesh <nazanin_binesh@yahoo.com>
2022-04-05 01:51:34 -0700

    back bottun on airportflight window

--- MayNazMuth/AirportFlightReportWindow.xaml.cs
+++ MayNazMuth/AirportFlightReportWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MayNazMuth.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,61 @@ namespace MayNazMuth
         public AirportFlightReportWindow()
         {
             InitializeComponent();
+
+            //turn the event handlers off
+            ToggleEventHandlers(false);
+
+            //Initialize the data grid
+            SetupGrid();
+
+            //populate the grid
+            Updategrid();
+
+            //Turn event handlers on
+            ToggleEventHandlers(true);
+        }
+
+        private void ToggleEventHandlers(bool toggle)
+        {
+            if (toggle)
+            {
+                //turn on                               
+                backToAirportButon.Click += backToAirportList;                                
+            }
+            else
+            {
+                //Turn off               
+                backToAirportButon.Click -= backToAirportList;
+            }
+        }
+
+        private void backToAirportList(object s, EventArgs e)
+        {
+            AirportWindow Airport = new AirportWindow();
+            CloseAllWindows();
+            Airport.Show();
+        }
+        public void CloseAllWindows()
+        {
+            foreach (Window window in Application.Current.Windows)
+            {
+                window.Hide();
+            }
+        }
+        private void Updategrid()
+        {
+            using (var ctx = new CustomDbContext())
+            {
+                //airportsList = ctx.Airports.ToList<Airport>();
+                //AirportFlightGrid.ItemsSource = airportsList;
+            }
+        }
+
+        private void SetupGrid()
+        {
+            AirportFlightGrid.SelectionMode = DataGridSelectionMode.Single;
+            //Make it read only
+            AirportFlightGrid.IsReadOnly = true;
         }
     }
 }
