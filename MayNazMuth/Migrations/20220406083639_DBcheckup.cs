deb6752d6b9119318a640ff2e8afcc187744633c
nazaninbinesh <nazanin_binesh@yahoo.com>
2022-04-06 20:05:50 -0700

    db problem solved

---
+++ MayNazMuth/Migrations/20220406083639_DBcheckup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MayNazMuth.Migrations
+{
+    public partial class DBcheckup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
