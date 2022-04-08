7b528c5046ad601581483029efe882795f6fbe43
95sainimayank <95sainimayank@gmail.com>
2022-04-08 01:27:04 -0700

    Added update passenger window

--- MayNazMuth/AddPassengerWindow.xaml.cs
+++ MayNazMuth/AddPassengerWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MayNazMuth.Entities;
+using MayNazMuth.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +14,94 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace MayNazMuth
-{
-    /// <summary>
-    /// Interaction logic for AddPassengerWindow.xaml
-    /// </summary>
-    public partial class AddPassengerWindow : Window
-    {
-        public AddPassengerWindow()
-        {
+namespace MayNazMuth {
+    public partial class AddPassengerWindow : Window {
+        public AddPassengerWindow() {
             InitializeComponent();
+
+            InitializeDataGrid();
+
+            btnAddPassenger.Click += AddPassenger;
+            btnDeletePassenger.Click += DeletePassenger;
         }
+
+        public void InitializeDataGrid() {
+            using(var db = new CustomDbContext()) {
+                AllPassengersDataGrid.ItemsSource =  db.Passengers.ToList();
+
+                db.SaveChanges();
+
+            }
+        }
+
+        public void AddPassenger(object sender, EventArgs args) {
+            string FullName = txtFullName.Text;
+            string Email = txtFullName.Text;
+            string PhoneNumber = txtPhoneNumber.Text;
+            string DateOfBirth = dateOfBirth.Text;
+            string Gender = gender.Text;
+            string PassportNumber = txtPassportNumber.Text;
+
+            if(isValid(FullName, Email, PhoneNumber, DateOfBirth, Gender, PassportNumber)) {
+                using(var db = new CustomDbContext()) {
+                    Passenger newPassenger = new Passenger(
+                                            FullName, 
+                                            PassportNumber,
+                                            Email,
+                                            PhoneNumber,
+                                            Convert.ToDateTime(DateOfBirth),
+                                            Gender);
+
+                    db.Passengers.Add(newPassenger);
+
+                    db.SaveChanges();
+                    InitializeDataGrid();
+                }
+            }
+            else {
+                MessageBox.Show("One or two fields values are not correct!");
+            }
+
+        }
+
+        public bool isValid(string fullName, string email, string phone, string dob, string gender, string passport) {
+
+            if (fullName.Equals("")) {
+                return false;
+            }
+            else if (email.Equals("")) {
+                return false;
+            }
+            else if (!phone.All(char.IsDigit)) {
+                return false;
+            }
+            else if (dob.Equals("")) {
+                return false;
+            }
+            else if (passport.Equals("")) {
+                return false;
+            }
+            else if (gender.Equals("")) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void DeletePassenger(object sender, EventArgs args) {
+            using (var db = new CustomDbContext()) {
+                var selectedPassengers = AllPassengersDataGrid.SelectedItems;
+
+                foreach(Passenger p in selectedPassengers) {
+                    db.Passengers.Remove(p);
+                }
+
+                db.SaveChanges();
+                InitializeDataGrid();
+            }
+        }
+
     }
+
+    
 }
