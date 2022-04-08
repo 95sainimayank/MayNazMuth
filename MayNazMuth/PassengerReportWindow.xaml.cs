dbbdad6959552ea224844a2971fe78992922a3fb
95sainimayank <95sainimayank@gmail.com>
2022-04-08 14:42:24 -0700

    Completed update passenger window

--- MayNazMuth/PassengerReportWindow.xaml.cs
+++ MayNazMuth/PassengerReportWindow.xaml.cs
@@ -19,15 +19,46 @@ namespace MayNazMuth {
         public PassengerReportWindow() {
             InitializeComponent();
 
+            InitializeDataGrid();
+
             btnSearch.Click += searchData;
+        }
+
+        public void InitializeDataGrid() {
+            using(var db = new CustomDbContext()) {
+                var query = db.Passengers
+                              .Join(db.BookingPassengers,
+                                    passengers => passengers.PassengerId,
+                                    pid => pid.PassengerId,
+                                    (passengers, pid) => new {
+                                        passengers, pid
+                                    })
+                              .Join(db.Bookings,
+                                     bookings => bookings.pid.BookingId,
+                                     bid => bid.BookingId,
+                                     (bookings, bid) => new {
+                                         bookings, bid
+                                     })
+                              .Select(m => new {
+                                  passengerName = m.bookings.passengers.FullName,
+                                  passengerPhone = m.bookings.passengers.PhoneNo,
+                                  passengerPassport = m.bookings.passengers.PassportNo,
+                                  flightArrival = m.bid.Flight.ArrivalTime,
+                                  flightDeparture = m.bid.Flight.DepartureTime,
+                                  flightArrivalAirport = m.bid.Flight.DestinationAirport.AirportName,
+                                  flightDepartureAirport = m.bid.Flight.SourceAirport.AirportName,
+                                  bookingDateTime = m.bid.BookingDatetime,
+                              });
+
+                PassengerReportDatagrid.ItemsSource =  query.ToList();
+            }
         }
 
         public void searchData(object sender, EventArgs args) {
             using (var db = new CustomDbContext()) {
-                string name = txtPassengerName.Text;
-                string contactNo = txtPassengerContact.Text;
-                string passport = txtPassengerPassport.Text;
-
+                string name = txtPassengerName.Text.Trim();
+                string contactNo = txtPassengerContact.Text.Trim();
+                string passport = txtPassengerPassport.Text.Trim();
 
                 var query = db.Passengers
                               .Join(db.BookingPassengers,
@@ -62,5 +93,7 @@ namespace MayNazMuth {
             }
 
         }
+
+        
     }
 }
--- MayNazMuth/UpdatePassengerWindow.xaml.cs
+++ MayNazMuth/UpdatePassengerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MayNazMuth.Utilities;
+﻿using MayNazMuth.Entities;
+using MayNazMuth.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,14 @@ namespace MayNazMuth {
     public partial class UpdatePassengerWindow : Window {
         public UpdatePassengerWindow() {
             InitializeComponent();
+
+            AllPassengerDataGrid.SelectionMode = (DataGridSelectionMode)SelectionMode.Single;
+            AllPassengerDataGrid.SelectionChanged += PopulateForm;
+            btnUpdatePassenger.Click += UpdatePassenger;
+
             InitializeDataGrid();
         }
+
         public void InitializeDataGrid() {
             using (var db = new CustomDbContext()) {
                 AllPassengerDataGrid.ItemsSource = db.Passengers.ToList();
@@ -27,7 +34,84 @@ namespace MayNazMuth {
             }
         }
 
+        public void PopulateForm(object sender, EventArgs args) {
+                Passenger passenger = (Passenger)AllPassengerDataGrid.SelectedItem;
 
+            lblPassengerId.Content = passenger.PassengerId;
+            txtFullName.Text = passenger.FullName;
+            txtEmail.Text = passenger.Email;
+            txtPassport.Text = passenger.PassportNo;
+            txtPhone.Text = passenger.PhoneNo;
+            txtDate.SelectedDate = passenger.DateOfBirth;
+
+            switch (passenger.Gender) {
+                case "Male":
+                    comboGender.SelectedIndex = 0;
+                    break;
+                case "Female":
+                    comboGender.SelectedIndex = 1;
+                    break;
+                case "Other":
+                    comboGender.SelectedIndex = 2;
+                    break;
+                default:
+                    comboGender.SelectedIndex = 0;
+                    break;
+            }
+
+        }
+
+        public void UpdatePassenger(object sender, EventArgs args) {
+            Console.WriteLine();
+
+            if(isValid(txtFullName.Text.Trim(), txtEmail.Text.Trim(), txtPhone.Text.Trim(), txtDate.SelectedDate.ToString().Trim(), comboGender.SelectedItem.ToString().Trim(), txtPassport.Text.Trim())){
+                using (var db = new CustomDbContext()) {
+                    Passenger updatedPassenger = new Passenger(
+                                        txtFullName.Text.Trim(),
+                                        txtPassport.Text.Trim(),
+                                        txtEmail.Text.Trim(),
+                                        txtPhone.Text.Trim(),
+                                        Convert.ToDateTime(txtDate.SelectedDate),
+                                        ((ComboBoxItem)comboGender.SelectedItem).Content.ToString());
+
+                    updatedPassenger.PassengerId = Convert.ToInt32(lblPassengerId.Content);
+                    //updatedPassenger.BookingPassengers = 
+                    db.Update(updatedPassenger);
+                    db.SaveChanges();
+
+                    AllPassengerDataGrid.SelectionChanged -= PopulateForm;
+                    InitializeDataGrid();
+                    AllPassengerDataGrid.SelectionChanged += PopulateForm;
+                }
+            }
+            else {
+                MessageBox.Show("One or two fields values are not correct!");
+            }
+        }
+
+        public bool isValid(string fullName, string email, string phone, string dob, string gender, string passport) {
+
+            if (fullName.Trim().Equals("")) {
+                return false;
+            }
+            else if (email.Trim().Equals("")) {
+                return false;
+            }
+            else if (!phone.Trim().All(char.IsDigit) || phone.Trim().Equals("")) {
+                return false;
+            }
+            else if (dob.Trim().Equals("")) {
+                return false;
+            }
+            else if (passport.Trim().Equals("")) {
+                return false;
+            }
+            else if (gender.Trim().Equals("")) {
+                return false;
+            }
+
+            return true;
+        }
 
 
 
